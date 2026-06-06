47ec465acf6be5cc8e0d9a9b47578305b96dca3d
linux_china <libing.chen@gmail.com>
2026-06-06 09:29:24 +0800

    chore: polishing

--- src/main/kotlin/org/mvnsearch/jetbrains/amber/terminal/AmberTerminalService.kt
+++ src/main/kotlin/org/mvnsearch/jetbrains/amber/terminal/AmberTerminalService.kt
@@ -7,20 +7,29 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.terminal.frontend.toolwindow.TerminalToolWindowTabsManager
 import com.intellij.terminal.frontend.view.TerminalView
+import org.jetbrains.plugins.terminal.TerminalToolWindowPanel
 
 @Service(Service.Level.PROJECT)
 class AmberTerminalService(val project: Project) {
 
     fun getOrCreateTerminalView(title: String): TerminalView {
         val tabsManager = TerminalToolWindowTabsManager.getInstance(project)
-        val toolWindow = ToolWindowManager.getInstance(project).getToolWindow("Terminal")!!
-        toolWindow.contentManager.findContent(title)?.let {
-            if (it.component is TerminalView) {
-                toolWindow.contentManager.setSelectedContent(it, true)
-                toolWindow.activate(null)
-                return it.component as TerminalView
-            }
+        val targetTab = tabsManager.tabs.find { it.content.tabName == title }
+        if (targetTab != null) {
+            val toolWindow = ToolWindowManager.getInstance(project).getToolWindow("Terminal")!!
+            toolWindow.contentManager.setSelectedContent(targetTab.content, true)
+            toolWindow.activate(null)
+            return targetTab.view
         }
+//        val toolWindow = ToolWindowManager.getInstance(project).getToolWindow("Terminal")!!
+//        toolWindow.contentManager.findContent(title)?.let {
+//            if(it.component is TerminalToolWindowPanel) {
+//                toolWindow.contentManager.setSelectedContent(it, true)
+//                toolWindow.activate(null)
+//                val terminalPanel = it.component as TerminalToolWindowPanel
+//
+//            }
+//        }
         return tabsManager.createTabBuilder()
             .tabName(title)
             .createTab()
