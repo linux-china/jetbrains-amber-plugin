83e8a3ab58bc18848c20bdf600f9a68b575331be
linux_china <libing.chen@gmail.com>
2026-06-07 15:11:53 +0800

    build: add patchPluginXml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
+import org.jetbrains.changelog.Changelog
+import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
     id("org.jetbrains.kotlin.jvm")
@@ -34,6 +38,35 @@ tasks {
         pathToParser.set("/org/mvnsearch/jetbrains/amber/parser/AmberParser.java")
         pathToPsiRoot.set("/org/mvnsearch/jetbrains/amber/psi")
         purgeOldFiles.set(true)
+    }
+
+    patchPluginXml {
+        version = properties("version")
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
+
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+        pluginDescription.set(
+            projectDir.resolve("README.md").readText().lines().run {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
+
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end))
+            }.joinToString("\n").run { markdownToHTML(this) }
+        )
+
+        // Get the latest available change notes from the changelog file
+        changeNotes.set(provider {
+            with(changelog) {
+                renderItem(
+                    getOrNull(properties("pluginVersion")) ?: getLatest(),
+                    Changelog.OutputType.HTML,
+                )
+            }
+        })
     }
 }
 
