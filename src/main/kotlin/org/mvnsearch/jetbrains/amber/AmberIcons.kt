b19d24e06466f1312e64c3a3ec1e7d8d5590a190
linux_china <libing.chen@gmail.com>
2026-06-07 22:53:19 +0800

    chore: add LayeredIcon

--- src/main/kotlin/org/mvnsearch/jetbrains/amber/AmberIcons.kt
+++ src/main/kotlin/org/mvnsearch/jetbrains/amber/AmberIcons.kt
@@ -1,8 +1,17 @@
 package org.mvnsearch.jetbrains.amber
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.IconLoader
+import com.intellij.ui.LayeredIcon
+import javax.swing.Icon
 
 object AmberIcons {
     @JvmField
-    val FILE = IconLoader.getIcon("/icons/amber-16x16.svg", AmberIcons::class.java)
+    val FILE: Icon = IconLoader.getIcon("/icons/amber-16x16.svg", AmberIcons::class.java)
+
+    @JvmField
+    val EXECUTABLE_FILE: Icon = LayeredIcon(2).apply {
+        setIcon(FILE, 0)
+        setIcon(AllIcons.Nodes.RunnableMark, 1)
+    }
 }
